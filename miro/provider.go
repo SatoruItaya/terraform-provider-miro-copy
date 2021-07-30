272d68abb115f3d5b157ab39dc3289cf3bdbe8a1
SatoruItaya <str.itaya@gmail.com>
2021-07-30 17:18:20 +0900

    implemente resources

--- miro/provider.go
+++ miro/provider.go
@@ -1,26 +1,32 @@
 package miro
 
 import (
+	"context"
+
+	"github.com/Miro-Ecosystem/go-miro/miro"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func Provider() *scema.Provider {
+func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
 			"access_token": {
 				Type:        schema.TypeString,
 				Description: "Access key for Miro API",
 				Required:    true,
-				Swnsitive:   true,
+				Sensitive:   true,
 			},
 		},
-		ResourcesMap:         map[string]*schema.Resource{},
+		ResourcesMap: map[string]*schema.Resource{
+			"miro_board": resourceBoard(),
+		},
 		DataSourcesMap:       map[string]*schema.Resource{},
 		ConfigureContextFunc: providerConfigureFunc,
 	}
 }
 
-func providerConfigureFunc(ctx context.Context, data *schema.ResorceData) (interface{}, diag.Diagnostics) {
+func providerConfigureFunc(ctx context.Context, data *schema.ResourceData) (interface{}, diag.Diagnostics) {
 	key := data.Get("access_token").(string)
 	var diags diag.Diagnostics
 	return miro.NewClient(key), diags
